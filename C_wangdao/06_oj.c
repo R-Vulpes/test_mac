e22aa2829c877e63503096f8da00bf2ace4dbcc9
R-Vulpes <1982747519@qq.com>
2024-03-16 20:27:05 +0800

    test06

--- C_wangdao/06_oj.c
+++ C_wangdao/06_oj.c
@@ -4,9 +4,11 @@
 int main()
 {
   int len = 0;
+  char d = 'a';
   scanf("%d", &len);
   char *c = (char*)malloc(len);
-  gets(c);
+  scanf("%c", &d); //此处用来读走缓冲区的\n，去掉结果会错误
+  fgets(c,len,stdin); //gets函数容易访问越界
   puts(c);
   free(c);
 //   system("pause");
