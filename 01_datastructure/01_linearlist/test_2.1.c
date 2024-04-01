917c2a4cc98219a60cfd24351e7c8c05bb35a5fe
R-Vulpes <1982747519@qq.com>
2024-04-01 16:18:39 +0800

    change

---
+++ 01_datastructure/01_linearlist/test_2.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void test(int x) // (int &x)可以将修改值带回
+{
+    x = 1024;
+    printf("函数内部 x=%d\n",x);
+}
+
+int main()
+{
+    int x = 1;
+    printf("调用前 x=%d\n",x);
+    test(x); 
+    printf("调用后 x=%d\n",x);
+
+    // system("pause");
+    return 0;
+}
